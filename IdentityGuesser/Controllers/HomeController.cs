8cbb23c9d3e13a78cfccd0a7b6796a8b927b3486
Maks Fisli <mfisli2@gmail.com>
2017-03-29 13:32:01 -0700

    Able to save images. Next: same path of image to model
    Signed-off-by: Maks Fisli <mfisli2@gmail.com>

--- IdentityGuesser/Controllers/HomeController.cs
+++ IdentityGuesser/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -26,5 +29,26 @@ namespace IdentityGuesser.Controllers
 
             return View();
         }
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                // Make sure the user selected an image file
+                if (!file.ContentType.StartsWith("image"))
+                {
+                    TempData["Message"] = "Only image files may be uploaded";
+                    return RedirectToAction("Index");
+                }
+                ViewBag.FileName = file.FileName;
+                string pic = System.IO.Path.GetFileName(file.FileName);
+                string path = System.IO.Path.Combine(
+                                   Server.MapPath("~/Images"), pic); 
+                // file is uploaded
+                file.SaveAs(path);
+            }
+            return View("Index");
+        }
     }
+
 }
---
+++ IdentityGuesser/Models/PoemModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IdentityGuesser.Models
+{
+    public class PoemModel
+    {
+        [Key]
+        public int PoemModelNumber { get; set; }
+        public string ImagePath { get; set; }
+        public string Age { get; set; }
+        public string Gender { get; set; }
+        public string Caption { get; set; }
+        public IEnumerable<String> Tags { get; set; }
+    }
+}
