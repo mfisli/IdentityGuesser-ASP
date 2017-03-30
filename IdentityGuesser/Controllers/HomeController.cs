7bf3b4a233043695752165820841d3a67a4f291d
Maks Fisli <mfisli2@gmail.com>
2017-03-29 20:08:57 -0700

    Bot is able to see age and gender. Also returns a caption and tags. Todo: populate the model.
    Signed-off-by: Maks Fisli <mfisli2@gmail.com>

--- IdentityGuesser/Controllers/HomeController.cs
+++ IdentityGuesser/Controllers/HomeController.cs
@@ -6,6 +6,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Vision;
+using System.Web.Configuration;
+using System.Diagnostics;
+using Microsoft.ProjectOxford.Vision.Contract;
 
 namespace IdentityGuesser.Controllers
 {
@@ -30,25 +35,62 @@ namespace IdentityGuesser.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Upload(HttpPostedFileBase file)
+        public async Task<ActionResult> Upload(HttpPostedFileBase file)
         {
             if (file != null && file.ContentLength > 0)
             {
+                var age = string.Empty;
+                var gender = string.Empty;
+                var caption = string.Empty;
+                List<string> tags = new List<string>();
                 // Make sure the user selected an image file
                 if (!file.ContentType.StartsWith("image"))
                 {
                     TempData["Message"] = "Only image files may be uploaded";
                     return RedirectToAction("Index");
                 }
-                ViewBag.FileName = file.FileName;
+
                 string pic = System.IO.Path.GetFileName(file.FileName);
                 string path = System.IO.Path.Combine(
-                                   Server.MapPath("~/Images"), pic); 
+                                   Server.MapPath("~/Images"), pic);
                 // file is uploaded
                 file.SaveAs(path);
+                ViewBag.FileName = path;
+                Debug.WriteLine("path: " + path);
+                // Create Project Oxford Computer Vision API Service client
+                var SubscriptionKey = WebConfigurationManager.AppSettings["SubscriptionKey"];
+                VisionServiceClient VisionServiceClient = new VisionServiceClient(SubscriptionKey);
+                Debug.WriteLine("VisionServiceClient is created");
+
+                using (Stream imageFileStream = System.IO.File.OpenRead(path))
+                {
+                    // Analyze the image for features
+                    Debug.WriteLine("Calling VisionServiceClient.AnalyzeImageAsync()...");
+                    //VisualFeature.Description, VisualFeature.Faces
+                    VisualFeature[] visualFeatures = new VisualFeature[] 
+                    {
+                        VisualFeature.Faces, VisualFeature.Description
+                    };
+                    AnalysisResult analysisResult = await VisionServiceClient.AnalyzeImageAsync(imageFileStream, visualFeatures);
+                    age = analysisResult.Faces[0].Age.ToString();
+                    gender = analysisResult.Faces[0].Gender;
+                    caption = analysisResult.Description.Captions[0].Text;
+                    for (int i = 0; i < analysisResult.Description.Tags.Length; i++)
+                    {
+                        tags.Add(analysisResult.Description.Tags[i]);
+                    }
+                }
+                
+                string tempTags = string.Empty;
+                foreach(string tag in tags)
+                {
+                    tempTags += tag + " ";
+                }
+                ViewBag.Caption = "Age: " + age + " Gender: " + gender + " Caption: " + caption + " Tags: " + tempTags;
+
+
             }
             return View("Index");
         }
     }
-
 }
