3e983d92ba86b2957a7ad97aaa0fd46569b9b152
Maks Fisli <mfisli2@gmail.com>
2017-07-24 13:31:09 -0700

    Added loading gif
    Signed-off-by: Maks Fisli <mfisli2@gmail.com>

---
+++ IdentityGuesser/Scripts/loadingGif.js
@@ -0,0 +1,17 @@
+﻿function log(msg){
+    console.log(msg);
+}
+window.onload = function () {
+    log("Loaded custom js script");
+    var trigger = document.getElementsByClassName("uploadBtn")[0];
+    trigger.addEventListener("click", function(){
+        log("Trigger fired.");
+        var loading = document.getElementById("loading");
+        log(loading);
+        if (loading) {
+            log(loading);
+            var source = "https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif"
+            loading.innerHTML = "<img src=\'" + source + "\' alt='loading...'/>";
+        }   
+    });
+}
